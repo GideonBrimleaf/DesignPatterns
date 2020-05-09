db2b688dc3f9a4b4937aba320d102d09edbe8caf
GideonBrimleaf <chrisdryden1@gmail.com>
2020-05-09 12:55:24 +0100

    implemented subject methods

---
+++ src/observer_pattern_models/WeatherData.kt
@@ -0,0 +1,21 @@
+package observer_pattern_models
+
+class WeatherData(
+    private val observers:MutableList<Observer>,
+    private var temp:Float,
+    private var humidity:Float,
+    private var pressure:Float
+) : Subject {
+
+    override fun registerObserver(observer: Observer) {
+        observers.add(observer)
+    }
+
+    override fun removeObserver(observer: Observer) {
+        observers.remove(observer)
+    }
+
+    override fun notifyObservers() {
+        observers.forEach{it.update(temp, humidity, pressure)}
+    }
+}
