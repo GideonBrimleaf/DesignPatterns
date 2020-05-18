4d9cb2f08046af0d1c09ff1ed367d79f75dc7a66
GideonBrimleaf <chrisdryden1@gmail.com>
2020-05-18 11:23:11 +0100

    fixed issue with streaming to array

--- src/test/decorator_pattern_test/LowerCaseInputStreamTest.kt
+++ src/test/decorator_pattern_test/LowerCaseInputStreamTest.kt
@@ -1,6 +1,7 @@
 package test.decorator_pattern_test
 
 import decorator_pattern_models.java_io.LowerCaseInputStream
+import org.junit.Ignore
 import org.junit.Test
 import org.omg.CORBA.INTERNAL
 import java.io.BufferedInputStream
@@ -11,16 +12,15 @@ class LowerCaseInputStreamTest {
 
     @Test
     fun `Testing input stream`(){
-        var c:Int
         val thing = LowerCaseInputStream(
                     BufferedInputStream(
-                        FileInputStream("test.txt")
+                        FileInputStream("src/test/decorator_pattern_test/test.txt")
                     )
                 )
+        var c:Int
         var result = ""
-        while(thing.read() >=0){
-            val character = thing.read()
-            result += character.toChar()
+        while(thing.read().also { c = it } >= 0){
+            result += c.toChar()
         }
         thing.close()
         assertEquals("i know the decorator pattern there i rule!", result)
